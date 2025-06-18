1d5fcc6c7421b94069684064b75e38a5158130b0
MANH NGUYEN DUC <manhvttb2004@gmail.com>
2025-06-18 11:26:49 +0700

    [build]: thêm biểu đồ cho trang home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,6 +8,17 @@ import CardList from "./components/card-list";
 import LineChart from "./components/line-chart";
 
 import PieChart from "./components/pie-chart";
+import RatingBarChart from "./components/rating-bar-chart";
+import TopMoviesRatingBarChart from "./components/top-movies-rating-bar-chart";
+import MonthlyViewsBarChart from "./components/monthly-views-bar-chart";
+import TopUsersBarChart from "./components/top-users-bar-chart";
+import AvgWatchtimeUserBarChart from "./components/avg-watchtime-user-bar-chart";
+import TopMoviesViewsBarChart from "./components/top-movies-views-bar-chart";
+import ViewsByGenrePieChart from "./components/views-by-genre-pie-chart";
+import ViewsByPeriodCharts from "./components/views-by-period-charts";
+import Last30DaysViewsLineChart from "./components/last-30-days-views-line-chart";
+import RatingDistributionHistogram from "./components/rating-distribution-histogram";
+import TopMoviesLikesBarChart from "./components/top-movies-likes-bar-chart";
 
 const wrapperCol: ColProps = {
 	xs: 24,
@@ -17,26 +28,33 @@ const wrapperCol: ColProps = {
 	xl: 12,
 	xxl: 12,
 };
+
 export default function Home() {
 	return (
 		<BasicContent>
-			<Row gutter={[20, 20]}>
-				<Col span={24}>
-					<CardList />
-				</Col>
-				<Col span={24}>
-					<LineChart />
-				</Col>
-				<Col span={24}>
-					<Row justify="space-between" gutter={[20, 20]}>
-						<Col {...wrapperCol}>
-							<BarChart />
-						</Col>
-						<Col {...wrapperCol}>
-							<PieChart />
-						</Col>
-					</Row>
-				</Col>
+			<CardList />
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col {...wrapperCol}><MonthlyViewsBarChart /></Col>
+				<Col {...wrapperCol}><Last30DaysViewsLineChart /></Col>
+			</Row>
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col {...wrapperCol}><TopUsersBarChart /></Col>
+				<Col {...wrapperCol}><AvgWatchtimeUserBarChart /></Col>
+			</Row>
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col {...wrapperCol}><TopMoviesViewsBarChart /></Col>
+				<Col {...wrapperCol}><TopMoviesRatingBarChart /></Col>
+			</Row>
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col {...wrapperCol}><ViewsByGenrePieChart /></Col>
+				<Col {...wrapperCol}><TopMoviesLikesBarChart /></Col>
+			</Row>
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col span={24}><ViewsByPeriodCharts /></Col>
+			</Row>
+			<Row gutter={[20, 20]} style={{ marginTop: 20 }}>
+				<Col {...wrapperCol}><RatingBarChart /></Col>
+				<Col {...wrapperCol}><RatingDistributionHistogram /></Col>
 			</Row>
 		</BasicContent>
 	);
